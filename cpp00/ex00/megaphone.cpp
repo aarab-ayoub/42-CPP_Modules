1fa50755d32ead03ce41e0a192ddeec69cce5d76
ayaarab <ayoubaarab571@gmail.com>
2025-07-31 02:30:15 +0100

    starting

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayaarab <ayaarab@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 02:18:12 by ayaarab             #+#    #+#             */
+/*   Updated: 2025/07/31 02:18:14 by ayaarab            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac , char **av)
+{
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 1;
+	}
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			char *str = av[i];
+			for (int j = 0; str[j] ; j++)
+			{
+				str[j] = std::toupper(str[j]);
+			}
+			std::cout << str;
+		}
+		std::cout << "\n";
+		return 0;
+	}
+	
+}
---
+++ learning/cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// using namespace std;
+
+int main()
+{
+	int num = 423;
+
+	std::cout << num;
+	std::cout << "\n";
+	std::cout << "hello world\n" << num;
+	std::cout << "hello world2";
+	return 0;
+}
